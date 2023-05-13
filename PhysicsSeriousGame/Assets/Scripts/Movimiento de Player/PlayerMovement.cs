f365ee6429aef022a84d38a04396c3b8983f7f45
Antonio Sarmiento <antoniosaryon155@gmail.com>
2023-05-13 11:02:02 -0500

    hmm

--- PhysicsSeriousGame/Assets/Scripts/Movimiento de Player/PlayerMovement.cs
+++ PhysicsSeriousGame/Assets/Scripts/Movimiento de Player/PlayerMovement.cs
@@ -5,51 +5,50 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //Referencias a componentes
     private Rigidbody2D mRb;
     private CapsuleCollider2D mCollider;
-    private Vector3 mMoveInput = Vector3.zero;
     private Animator mAnimator;
+
+    //Variable para la Dirección de movimiento
+    private Vector3 mMoveInput;
+    
     [SerializeField]
     private float walkSpeed;
 
+    //-----------------------------------------------------
+
     void Awake()
     {
+        //Obtenemos referencias
         mRb = GetComponent<Rigidbody2D>();
         mCollider = GetComponent<CapsuleCollider2D>();
         mAnimator = GetComponent<Animator>();
     }
+    //-----------------------------------------------------
 
-    // Update is called once per frame
     private void Update()
     {
+        //Controlamos las animaciones
         Animations();
     }
-
+    //-----------------------------------------------------
     private void FixedUpdate()
     {
-        /*mRb.velocity = new Vector2(
-            mMoveInput.x, 
-            mMoveInput.y
-            ).normalized * walkSpeed;*/
-
+        //Movemos la posición del Player
         mRb.MovePosition(
             transform.position + (mMoveInput * walkSpeed * Time.fixedDeltaTime)
         );
         
     }
-
+    //-----------------------------------------------------
     private void OnMove(InputValue value)
     {
         //Almacenamos el Vector con la unidad de movimiento en X
         mMoveInput = value.Get<Vector2>().normalized;
-
-        /*mRb.velocity = new Vector2(
-            mMoveInput.x,
-            mMoveInput.y
-            ).normalized * walkSpeed;*/
-
     }
 
+    //-----------------------------------------------------
     private void Animations()
     {
         if(mMoveInput.magnitude != 0)
