7d13f92fcc1e23db40fbe3b3ef58d958b24ce046
Antonio Sarmiento <antoniosaryon155@gmail.com>
2023-05-18 02:25:08 -0500

    Mecanicas Listas

--- PhysicsSeriousGame/Assets/Scripts/Modo3D/UI/ButtonsManager.cs
+++ PhysicsSeriousGame/Assets/Scripts/Modo3D/UI/ButtonsManager.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ButtonsManager : MonoBehaviour
 {
+    [SerializeField] private GameObject CameraPlayer;
+
+    private TouchDeteccion PlayerTouchDetection;
+
     //Lista de botones de gravedad (hijos)
     [SerializeField] private List<GameObject> optsGravedad;
+
+    [SerializeField] private TextMeshProUGUI textMasa;
+    [SerializeField] private TextMeshProUGUI textVelocidad;
 
     //Flags de Status
     private bool gravedadActivada;
@@ -14,8 +22,21 @@ public class ButtonsManager : MonoBehaviour
 
     private void Awake()
     {
+        //Obtenemos referencias
+        PlayerTouchDetection = CameraPlayer.GetComponent<TouchDeteccion>();
+
         //Inicializamos el Flag en Falso
         gravedadActivada = false;
+    }
+
+    private void Update()
+    {
+        //Si hay un RigidBody seleccionado
+        if (PlayerTouchDetection.RigidBodySeleccionado != null)
+        {
+            textMasa.text = "Masa: " + PlayerTouchDetection.RigidBodySeleccionado.mass.ToString() + " kg.";
+            textVelocidad.text = "Velocidad: " + PlayerTouchDetection.RigidBodySeleccionado.velocity.magnitude.ToString("F2") + " m/s";
+        }
     }
 
     public void ControlarBotonesGravedad()
