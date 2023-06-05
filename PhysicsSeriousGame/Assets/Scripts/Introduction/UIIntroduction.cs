29cc53cbef4a8b0b35e89790d0b9bd905069dd5f
Antonio Sarmiento <antoniosaryon155@gmail.com>
2023-06-05 09:35:19 -0500

    Introduccion incorporada

--- PhysicsSeriousGame/Assets/Scripts/Introduction/UIIntroduction.cs
+++ PhysicsSeriousGame/Assets/Scripts/Introduction/UIIntroduction.cs
@@ -4,8 +4,52 @@ using UnityEngine;
 
 public class UIIntroduction : MonoBehaviour
 {
+    [SerializeField] private Animator animatorCientifico;
+    private Animator mAnimator;
+
+    private AudioSource mAudioSource;
+    [SerializeField] private AudioClip clipDespertando;
+    [SerializeField] private AudioClip clipEmpezandoJuego;
+    [SerializeField] private AudioClip clipLapiz;
+    [SerializeField] private AudioClip clipCRAB;
+
+
+    //-----------------------------------------------------
+
+    private void Awake()
+    {
+        mAnimator = GetComponent<Animator>();
+        mAudioSource = GetComponent<AudioSource>();
+    }
+
+    //-----------------------------------------------------
+
     public void Despertar()
     {
         ScenesManager.Instance.SolicitarCambioDeEscena("2DLabPrincipal");
+    }
+
+    public void ReproducirDITRO()
+    {
+        mAudioSource.PlayOneShot(clipDespertando, 0.75f);
+    }
+
+    public void ReproducirLapiz()
+    {
+        mAudioSource.PlayOneShot(clipLapiz, 0.5f);
+    }
+
+    public void ReproducirCRAB()
+    {
+        mAudioSource.PlayOneShot(clipCRAB, 0.5f);
+    }
+    public void ReproducirEmpezandoJuego()
+    {
+        mAudioSource.PlayOneShot(clipEmpezandoJuego, 0.75f);
+    }
+
+    public void DetenerLapiz()
+    {
+        mAudioSource.Stop();
     }
 }
