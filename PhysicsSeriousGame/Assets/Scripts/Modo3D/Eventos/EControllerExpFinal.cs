a03a61fc0e78951cf7c32e4e95ab180920c83c67
Antonio Sarmiento <antoniosaryon155@gmail.com>
2023-11-25 21:06:59 -0500

    Ajustes realizados

---
+++ PhysicsSeriousGame/Assets/Scripts/Modo3D/Eventos/EControllerExpFinal.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EControllerExpFinal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Seteamos la Gravedad a la Normal
+        Physics.gravity = new Vector3(0, -9.81f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
